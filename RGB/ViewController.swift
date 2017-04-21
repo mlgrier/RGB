e731aa66690125fa7bf05cf664da15efee157ec3
Marco Grier <mlgrier@gmail.com>
2017-04-20 22:16:09 -0500

    Updated code and added Outlets and Actions

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greyLabel: UILabel!
+    @IBOutlet weak var greySlider: UISlider!
+    
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func greyAction(_ sender: UISlider) {
+        
+        view.backgroundColor = UIColor(white: CGFloat(sender.value), alpha: 1)
     }
 
+    @IBAction func rgbAction(_ sender: UISlider) {
+        
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
 
 }
 
