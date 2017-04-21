b64d0545bffb833ff10b14433b8333fa7a0917e7
Marco Grier <mlgrier@gmail.com>
2017-04-20 22:56:10 -0500

    Final

--- RGB/ViewController.swift
+++ RGB/ViewController.swift
@@ -34,6 +34,18 @@ class ViewController: UIViewController {
         view.backgroundColor = UIColor(white: CGFloat(sender.value), alpha: 1)
         
         greyLabel.text = String(format: "%.2f", sender.value)
+        
+        if sender.value >= 0.5 {
+            greyLabel.textColor = UIColor(white: 0, alpha: 1)
+            redLabel.textColor = UIColor(white: 0, alpha: 1)
+            greenLabel.textColor = UIColor(white: 0, alpha: 1)
+            blueLabel.textColor = UIColor(white: 0, alpha: 1)
+        } else {
+            greyLabel.textColor = UIColor(white: 1, alpha: 1)
+            redLabel.textColor = UIColor(white: 1, alpha: 1)
+            greenLabel.textColor = UIColor(white: 1, alpha: 1)
+            blueLabel.textColor = UIColor(white: 1, alpha: 1)
+        }
     }
 
     @IBAction func rgbAction(_ sender: UISlider) {
@@ -48,9 +60,45 @@ class ViewController: UIViewController {
         greenLabel.text = String(format: "%.2f", greenSlider.value)
         blueLabel.text = String(format: "%.2f", blueSlider.value)
         
+        if redSlider.value >= 0.5 && greySlider.value >= 0.5 && blueSlider.value >= 0.5 {
+            redLabel.textColor = UIColor(white: 0, alpha: 1)
+            greenLabel.textColor = UIColor(white: 0, alpha: 1)
+            blueLabel.textColor = UIColor(white: 0, alpha: 1)
+            greyLabel.textColor = UIColor(white: 0, alpha: 1)
+        } else {
+            redLabel.textColor = UIColor(white: 1, alpha: 1)
+            greenLabel.textColor = UIColor(white: 1, alpha: 1)
+            blueLabel.textColor = UIColor(white: 1, alpha: 1)
+            greyLabel.textColor = UIColor(white: 1, alpha: 1)
+        }
+        
     }
 
     @IBAction func reset() {
+        greySlider.value = 0.50
+        redSlider.value = 0.50
+        greenSlider.value = 0.50
+        blueSlider.value = 0.50
+        
+        greyLabel.text = "0.50"
+        redLabel.text = "0.50"
+        greenLabel.text = "0.50"
+        blueLabel.text = "0.50"
+        
+        view.backgroundColor = UIColor(white: 0.5, alpha: 1)
+        
+        let transition =  CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 2
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.add(transition, forKey: nil)
     }
 }
+
+
+
+
+
+
+
 
